134cbc2e67d811bb30397a33deebe510435aacab
avd-web <avd-web@outlook.com>
2023-03-13 11:29:49 +0100

    backup working_files 3 - 13/03/2023

---
+++ src/com/arjanvandokkum/playersguidejava/backupscompleted/Day6TheFourSistersAndTheDuckBear_Final1.java
@@ -0,0 +1,36 @@
+package com.arjanvandokkum.playersguidejava.backupscompleted;
+
+import java.util.Scanner;
+
+public class Day6TheFourSistersAndTheDuckBear_Final1 {
+
+    //TODO Create a program that lets the user enter the number of chocolate eggs gathered that day.
+    //TODO Using / and %, compute how many eggs each sister should get and how many are left over for the duckbear.
+    //TODO Answer this question: What are three total egg counts where the duckbear gets more than each sister does? You can use the program you created to help you find the answer
+    final public static int PEOPLE = 4;
+
+    public static int createScanner() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("How many eggs are gathered today? : ");
+        int amountOfEggs = createScanner();
+
+        if (amountOfEggs > PEOPLE) {
+            int eggsPerSister = amountOfEggs / PEOPLE;
+            int eggsForDuckbear = amountOfEggs % PEOPLE;
+            if (eggsForDuckbear == 0) {
+                System.out.println("Each sister gets " + eggsPerSister + " eggs and duckbear gets none");
+            } else {
+                System.out.println("Each sisters gets " + eggsPerSister + " eggs and duckbear gets " + eggsForDuckbear);
+            }
+        }
+        else{
+            System.out.println("duckbear gets " + amountOfEggs + " eggs and sisters get none");
+        }
+
+
+    }
+}
---
+++ src/com/arjanvandokkum/playersguidejava/backupscompleted/Day7TheDominionOfKings_Final1.java
@@ -0,0 +1,56 @@
+package com.arjanvandokkum.playersguidejava.backupscompleted;
+
+import java.util.Scanner;
+
+public class Day7TheDominionOfKings_Final1 {
+
+    private static int amountOfEstates;
+    private static int amountOfDuchies;
+    private static int amountOfProvinces;
+    final static int ESTATE_POINTS = 1;
+    final static int DUCHY_POINTS = 3;
+    final static int PROVINCE_POINTS = 6;
+    private static int totalPoints;
+
+    public static int createScanner() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static void askUserInput() {
+        do {
+            System.out.println("How many estates does the kingdom have?");
+            amountOfEstates = createScanner();
+        }
+        while (amountOfEstates < 0 || amountOfEstates > 1_000);
+        System.out.println("The kingdom has " + amountOfEstates + " estates.");
+
+        do {
+            System.out.println("How many duchies does the kingdom have?");
+            amountOfDuchies = createScanner();
+        }
+        while (amountOfDuchies < 0 || amountOfDuchies > 1_000);
+        System.out.println("The kingdom has " + amountOfDuchies + " duchies.");
+
+        do {
+            System.out.println("How many provinces does the kingdom have?");
+            amountOfProvinces = createScanner();
+        }
+        while (amountOfProvinces < 0 || amountOfProvinces > 1_000);
+        System.out.println("The kingdom has " + amountOfProvinces + " provinces");
+
+    }
+
+    public static void calculatePoints(){
+        totalPoints = (amountOfEstates*ESTATE_POINTS) + (amountOfDuchies*DUCHY_POINTS) + (amountOfProvinces*PROVINCE_POINTS);
+    }
+
+    public static void main(String[] args) {
+        askUserInput();
+        calculatePoints();
+        System.out.println("<-------------------------------------------->");
+        System.out.println("The kingdom has a total points value of " + totalPoints + ".");
+
+    }
+
+}
